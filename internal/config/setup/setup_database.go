6dfeaf2be068461b00199cef05a29bac10f0faa9
XiaoLFeng <gm@x-lf.cn>
2025-05-25 16:13:43 +0800

    feat(config): 增加数据库初始化配置功能
    增加 `DatabaseSetup` 方法，支持数据库表的自动检查与初始化；若表不存在，则通过解析并执行 SQL 文件来完成创建，增强系统初始化能力。

--- internal/config/setup/setup_database.go
+++ internal/config/setup/setup_database.go
@@ -23,26 +23,50 @@ var databaseList = []string{
 	"xf_schedule.sql",             // 调度表（依赖线路、车辆和司机表）
 }
 
+// DatabaseSetup 完成数据库的初始化配置。
+//
+// 执行步骤包括:
+//   - 获取数据库连接信息
+//   - 检查指定的数据库表是否存在，若不存在则通过执行 SQL 文件来创建表
+//   - SQL 文件按需拆分并逐条执行，完成表结构的生成
+//
+// 错误:
+//   - 数据库配置错误或连接失败
+//   - SQL 文件解析或执行过程中出现的错误
 func (setup *SystemSetup) DatabaseSetup() {
 	// 1. 获取数据库连接
 	db := g.DB()
+	// 获取数据库名字
+	getName, configErr := g.Cfg().Get(setup.ctx, "database.default.name")
+	if configErr != nil {
+		panic(configErr)
+	}
 
 	// 2. 遍历数据库列表，执行 SQL 文件
 	for _, fileName := range databaseList {
-		getFile := g.Res().Get("template/sql/" + fileName)
-		getSql := string(getFile.Content())
-		// 拆分为单个 SQL 语句
-		sqlStatements := gstr.Split(getSql, ";")
-		for _, sql := range sqlStatements {
-			sql = gstr.Trim(sql)
-			if sql == "" {
-				continue
-			}
-			// 执行 SQL 语句
-			_, err := db.Exec(setup.ctx, sql)
-			if err != nil {
-				g.Log().Errorf(setup.ctx, "执行 SQL 失败: %s, 错误: %v", sql, err)
-				return
+		hasExist, sqlErr := db.Model("information_schema.TABLES").
+			Where("TABLE_SCHEMA", getName).
+			Where("TABLE_NAME", gstr.Trim(fileName, ".sql")).Exist()
+		if sqlErr != nil {
+			g.Log().Errorf(setup.ctx, "查询数据库表 %s 失败: %v", fileName, sqlErr)
+			return
+		}
+		if !hasExist {
+			getFile := g.Res().Get("template/sql/" + fileName)
+			getSql := string(getFile.Content())
+			// 拆分为单个 SQL 语句
+			sqlStatements := gstr.Split(getSql, ";")
+			for _, sql := range sqlStatements {
+				sql = gstr.Trim(sql)
+				if sql == "" {
+					continue
+				}
+				// 执行 SQL 语句
+				_, err := db.Exec(setup.ctx, sql)
+				if err != nil {
+					g.Log().Errorf(setup.ctx, "执行 SQL 失败: %s, 错误: %v", sql, err)
+					return
+				}
 			}
 		}
 	}
